a373b67415b315ee77a33ef81b60080b5956d1f5
Jay <jaygala@habana.ai>
2025-02-16 13:44:09 +0200

    fix training loop

--- train.py
+++ train.py
@@ -1,15 +1,23 @@
 import torch
 import re
 from gpt import SimpleTokenizer, GPTModel, PanchatantraDataset
+import habana_frameworks.torch.core as htcore
+import habana_frameworks.torch.hpu.graphs as htgraphs
+import habana_frameworks.torch.hpu as hthpu
+
+if torch.hpu.is_available():
+    device = "hpu"
+else:
+    device = "cpu"
 
 with open('panchatantra.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # result = re.findall(r'\w+|[^\w\s]', text)
 preprocessed_text = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-print(preprocessed_text[:10])
+# print(preprocessed_text[:10])
 preprocessed_text = [item.strip() for item in preprocessed_text]
-print(preprocessed_text[:10])
+# print(preprocessed_text[:10])
 
 all_words = sorted(list(set(preprocessed_text)))
 all_words.extend(["<|end_of_text|>", "<|unk|>"])
@@ -23,40 +31,36 @@ from torch.utils.data import DataLoader
 
 train_dataloader = DataLoader(dataset, batch_size=8, shuffle=True, drop_last=True)
 
-print(dataset[0])
+# print(dataset[0])
 
 model_config = {
     "vocab_size": 1110,     # Vocabulary size
     "context_length": 16,  # Context length
     "emb_dim": 768,          # Embedding dimension
-    "n_heads": 4,           # Number of attention heads
-    "n_layers": 4,          # Number of layers
+    "n_heads": 16,           # Number of attention heads
+    "n_layers": 16,          # Number of layers
     "drop_rate": 0.1,        # Dropout rate
     "qkv_bias": False 
 }
 
-learning_rate = 1e-4
-num_epochs = 1
+learning_rate = 1e-3
+num_epochs = 5
 
 model = GPTModel(cfg=model_config)
+model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 loss_fn = torch.nn.CrossEntropyLoss()
 
 for epoch in range(num_epochs):
     for batch in train_dataloader:
         inputs, outputs = batch
+        inputs.to(device)
+        outputs.to(device)
 
-        for i in range(len(inputs)):
-            x = inputs[:i+1] # get the inputs
-            y = outputs[i]
-            logits = model(x)
-            # print(logits.shape)
-            predicted = torch.argmax(logits, dim=-1, keepdim=True)
-            print(predicted.shape, y.shape)
-            loss = loss_fn(predicted.to(torch.float32), y.unsqueeze(0))
+        optimizer.zero_grad()
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        logits = model(inputs)
+    
+        loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1).to(device), outputs.flatten())
 
-            print("Loss:", loss.item())
+        print("Loss:",loss.item())
