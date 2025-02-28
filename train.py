87a2218e4c0b5da635c87bd7bf578717ad28f6c3
Jay Gala <jaygala260@gmail.com>
2025-02-28 18:35:08 +0530

    add evaluation, graphs

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import torch
 import re
+from tqdm import tqdm
 from gpt import SimpleTokenizer, GPTModel, PanchatantraDataset
 import habana_frameworks.torch.core as htcore
 import habana_frameworks.torch.hpu.graphs as htgraphs
 import habana_frameworks.torch.hpu as hthpu
+import matplotlib.pyplot as plt
 
 if torch.hpu.is_available():
     device = "hpu"
@@ -27,13 +29,19 @@ all_words.extend(["<|end_of_text|>", "<|unk|>"])
 vocab = {token: i for i, token in enumerate(all_words)}
 
 tokenizer = SimpleTokenizer(vocab=vocab)
-dataset = PanchatantraDataset(input=preprocessed_text, tokenizer=tokenizer, context_length=8, stride=8)
+train_ratio = 0.8
+test_ratio = 1 - train_ratio
+train_size = int(len(preprocessed_text) * train_ratio)
+train_data = preprocessed_text[:train_size]
+test_data = preprocessed_text[train_size:]
+
+train_dataset = PanchatantraDataset(input=train_data, tokenizer=tokenizer, context_length=8, stride=8)
+test_dataset = PanchatantraDataset(input=test_data, tokenizer=tokenizer, context_length=8, stride=8)
 
 from torch.utils.data import DataLoader
 
-train_dataloader = DataLoader(dataset, batch_size=8, shuffle=True, drop_last=True)
-
-# print(dataset[0])
+train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True, drop_last=True)
+test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=False, drop_last=False) #Added test dataloader
 
 model_config = {
     "vocab_size": 1110,     # Vocabulary size
@@ -53,11 +61,15 @@ model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 loss_fn = torch.nn.CrossEntropyLoss()
 
+train_losses = []
+test_losses = []
+
 for epoch in range(num_epochs):
 
     print("Epoch:", epoch)
+    train_loss = 0
 
-    for i,batch in enumerate(train_dataloader):
+    for i,batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
         inputs, outputs = batch
         inputs.to(device)
         outputs.to(device)
@@ -68,13 +80,43 @@ for epoch in range(num_epochs):
     
         loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1).to(device), outputs.flatten())
 
-        # print("Loss:",loss.item())
-
         loss.backward()
         optimizer.step()
+        train_loss += loss.item()
 
         if i%5 == 0:
             print(f"Loss at {i}: {loss.item()}")
+    
+    #Testing Logic
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, batch in enumerate(test_dataloader):
+            inputs, outputs = batch
+            inputs = inputs.to(device)
+            outputs = outputs.to(device)
+            logits = model(inputs)
+            loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), outputs.flatten())
+            test_loss += loss.item()
+    
+    avg_train_loss = train_loss / len(train_dataloader)
+    avg_test_loss = test_loss / len(test_dataloader)
+    train_losses.append(avg_train_loss)
+    test_losses.append(avg_test_loss)
+    print(f"Train Loss for epoch {epoch}: {avg_train_loss}")
+    print(f"Test Loss for epoch {epoch}: {avg_test_loss}")
+    model.train() #Switch back to training mode
+
+#Plot the graph
+epochs = range(num_epochs)
+plt.plot(epochs, train_losses, label='Train Loss')
+plt.plot(epochs, test_losses, label='Test Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Train Loss vs Test Loss')
+plt.legend()
+plt.show()
+
 
 print("yay")
 
